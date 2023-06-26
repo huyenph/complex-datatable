b1b820245e099937c4715954de1120fe2c083b5c
HuyenPham <huyen.phamhoang96@gmail.com>
2023-06-26 14:13:55 +0700

    new: config webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
