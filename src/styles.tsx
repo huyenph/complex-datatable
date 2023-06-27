ce16a61c0250920275a99fab2b4b815f4e8dfa02
HuyenPham <huyen.phamhoang96@gmail.com>
2023-06-27 10:40:07 +0700

    fix: export component data

--- src/styles.tsx
+++ src/styles.tsx
@@ -6,18 +6,16 @@ import {
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const StyledTableCell = styled(TableCell)<TableCellProps>(
-  ({ theme }) => ({
-    textTransform: "initial",
-    color: theme.palette.common.black,
-  })
-);
+const StyledTableCell = styled(TableCell)<TableCellProps>(({ theme }) => ({
+  textTransform: "initial",
+  color: theme.palette.common.black,
+}));
 
-export const StyledTableBody = styled(TableBody)<TableBodyProps>(
-  ({ theme }) => ({
-    textTransform: "initial",
-    "& .MuiTableCell-body": {
-      color: theme.palette.grey[800],
-    },
-  })
-);
+const StyledTableBody = styled(TableBody)<TableBodyProps>(({ theme }) => ({
+  textTransform: "initial",
+  "& .MuiTableCell-body": {
+    color: theme.palette.grey[800],
+  },
+}));
+
+export { StyledTableCell, StyledTableBody };
