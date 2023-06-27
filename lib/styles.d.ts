fe68992f3eeb3795b39277d39334ebf94dfc8c0f
HuyenPham <huyen.phamhoang96@gmail.com>
2023-06-27 14:48:46 +0700

    refactor: webpack

--- lib/styles.d.ts
+++ lib/styles.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="react" />
-import { TableCellProps } from "@mui/material";
+import { TableCellProps } from '@mui/material';
 declare const StyledTableCell: import("@emotion/styled").StyledComponent<TableCellProps & import("@mui/system").MUIStyledCommonProps<import("@mui/material").Theme>, {}, {}>;
 declare const StyledTableBody: import("@emotion/styled").StyledComponent<{
-    children?: any;
+    children?: import("react").ReactNode;
     classes?: Partial<import("@mui/material").TableBodyClasses> | undefined;
     sx?: import("@mui/material").SxProps<import("@mui/material").Theme> | undefined;
 } & import("@mui/material/OverridableComponent").CommonProps & Omit<Omit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLTableSectionElement>, HTMLTableSectionElement>, "ref"> & {
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
+const path = require('path');
+
 module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  resolve: {
+    modules: [__dirname, 'node_modules'],
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+  },
   module: {
     rules: [
       {
         test: /\.ts|\.tsx$/,
-        use: 'awesome-typescript-loader',
+        use: 'babel-loader',
         include: __dirname,
       },
     ],
