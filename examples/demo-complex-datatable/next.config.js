5f8de43090771769cec7af04238bcd7093c47354
HuyenPham <huyen.phamhoang96@gmail.com>
2023-08-04 10:40:13 +0700

    Update examples

--- examples/demo-complex-datatable/next.config.js
+++ examples/demo-complex-datatable/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  compiler: {
+    styledComponents:
+      true |
+      {
+        displayName: true,
+        ssr: true,
+      },
+  },
 };
 
 module.exports = nextConfig;
