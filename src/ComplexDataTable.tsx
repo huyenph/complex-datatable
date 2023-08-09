24ed39823e2e5cf5c2b695c121af6e4f9e5cd924
HuyenPham <huyen.phamhoang96@gmail.com>
2023-08-09 10:51:22 +0700

    Update compare objects

--- src/ComplexDataTable.tsx
+++ src/ComplexDataTable.tsx
@@ -12,22 +12,26 @@ import {
   Typography,
   Checkbox,
   Card,
+  IconButton,
+  Collapse,
+  SxProps,
 } from '@mui/material';
+import { KeyboardArrowDown } from '@mui/icons-material';
 import { visuallyHidden } from '@mui/utils';
 import { HeadCell } from './types';
-import { StyledTableBody, StyledTableCell } from './styles';
+import { StyledTableBody, StyledTableCell, StyledCollapseTableCell } from './styles';
+
+type Order = 'asc' | 'desc';
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
+  if (JSON.stringify(b[orderBy]) < JSON.stringify(a[orderBy])) {
     return -1;
   }
-  if (b[orderBy] > a[orderBy]) {
+  if (JSON.stringify(b[orderBy]) > JSON.stringify(a[orderBy])) {
     return 1;
   }
   return 0;
 }
-
-type Order = 'asc' | 'desc';
 
 function getComparator<Key extends keyof any>(
   order: Order,
@@ -178,10 +182,10 @@ interface ComplexTableProps<T> {
   isSelecting?: boolean;
   selected?: string[];
   setSelected?: (value: string[]) => void;
-  renderRowCells: (row: T) => ReactNode;
-  renderCollapseRow?: (row: T) => ReactNode;
-  handleSelectAllClick?: (event: React.ChangeEvent<HTMLInputElement>) => void | undefined;
-  handleClick?: (event: React.MouseEvent<unknown>, name: string) => void | undefined;
+  sx?: SxProps;
+  rowCells: (row: T) => ReactNode;
+  collapseTable?: (row: T) => ReactNode;
+  handleRowClick?: (event: React.MouseEvent<unknown>, id: string) => void | undefined;
   toolbarLabel?: string;
   toolbarAction?: ReactNode;
   enabledNavigate?: boolean;
@@ -195,18 +199,23 @@ export default function ComplexDataTable<T>(props: ComplexTableProps<T>) {
     rows,
     selected,
     setSelected,
-    renderRowCells,
-    renderCollapseRow,
-    handleSelectAllClick,
-    handleClick,
+    sx,
+    rowCells,
+    collapseTable,
+    handleRowClick,
     toolbarLabel,
     toolbarAction,
     enabledNavigate = true,
   } = props;
+
+  const [isOpenCollapse, setOpenCollapse] = useState<any>({});
+
   const [order, setOrder] = useState<Order>('asc');
   const [orderBy, setOrderBy] = useState<keyof T>(defaultKey as keyof T);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const enableSelected = selected !== undefined && setSelected !== undefined;
 
   const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof T) => {
     const isAsc = orderBy === property && order === 'asc';
@@ -214,12 +223,12 @@ export default function ComplexDataTable<T>(props: ComplexTableProps<T>) {
     setOrderBy(property);
   };
 
-  const onHandleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    if (selected && setSelected) {
-      const selectedIndex = selected.indexOf(name);
+  const onHandleRowClick = (event: React.MouseEvent<unknown>, id: string) => {
+    if (enableSelected) {
+      const selectedIndex = selected.indexOf(id);
       let newSelected: string[] = [];
       if (selectedIndex === -1) {
-        newSelected = newSelected.concat(selected, name);
+        newSelected = newSelected.concat(selected, id);
       } else if (selectedIndex === 0) {
         newSelected = newSelected.concat(selected.slice(1));
       } else if (selectedIndex === selected.length - 1) {
@@ -232,8 +241,8 @@ export default function ComplexDataTable<T>(props: ComplexTableProps<T>) {
       }
       setSelected(newSelected);
     }
-    if (handleClick) {
-      handleClick(event, name);
+    if (handleRowClick) {
+      handleRowClick(event, id);
     }
   };
 
@@ -244,6 +253,17 @@ export default function ComplexDataTable<T>(props: ComplexTableProps<T>) {
   const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
+  };
+
+  const onHandleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (enableSelected) {
+      if (event.target.checked) {
+        const newSelected = rows.map((n) => `${n.id}`);
+        setSelected(newSelected);
+        return;
+      }
+      setSelected([]);
+    }
   };
 
   const isSelected = (name: string) => (selected ? selected.indexOf(name) !== -1 : false);
@@ -257,15 +277,16 @@ export default function ComplexDataTable<T>(props: ComplexTableProps<T>) {
         page * rowsPerPage,
         page * rowsPerPage + rowsPerPage
       ),
-    [order, orderBy, page, rowsPerPage]
+    [order, orderBy, page, rows, rowsPerPage]
   );
 
   return (
     <Card
       sx={{
         width: '100%',
-        mb: renderCollapseRow === undefined ? 0 : 2,
-        boxShadow: renderCollapseRow === undefined ? 0 : undefined,
+        mb: collapseTable === undefined ? 0 : 2,
+        boxShadow: collapseTable === undefined ? 0 : undefined,
+        ...sx,
       }}
     >
       {toolbarLabel && (
@@ -288,30 +309,30 @@ export default function ComplexDataTable<T>(props: ComplexTableProps<T>) {
             numSelected={selected ? selected.length : 0}
             order={order}
             orderBy={orderBy as string}
-            onSelectAllClick={handleSelectAllClick}
+            onSelectAllClick={enableSelected ? onHandleSelectAllClick : undefined}
             onRequestSort={handleRequestSort}
             rowCount={rows.length}
           />
           <StyledTableBody>
             {visibleRows.map((row, index) => {
-              const isItemSelected = isSelected((row as any).name);
+              const isItemSelected = isSelected(`${(row as any).id}`);
               const labelId = `Complex-table-checkbox-${index}`;
 
               return (
                 <>
                   <TableRow
                     hover
-                    onClick={(event) => onHandleClick(event, (row as any).name)}
-                    role={handleSelectAllClick !== undefined ? 'checkbox' : 'list'}
+                    onClick={(event) => onHandleRowClick(event, `${(row as any).id}`)}
+                    role={enableSelected ? 'checkbox' : 'list'}
                     aria-checked={isItemSelected}
                     tabIndex={-1}
-                    key={(row as any).name}
+                    key={`${(row as any).id}`}
                     selected={isItemSelected}
                     sx={{
                       cursor: 'pointer',
                     }}
                   >
-                    {handleSelectAllClick !== undefined && (
+                    {enableSelected && (
                       <StyledTableCell padding="checkbox">
                         <Checkbox
                           color="primary"
@@ -322,9 +343,43 @@ export default function ComplexDataTable<T>(props: ComplexTableProps<T>) {
                         />
                       </StyledTableCell>
                     )}
-                    {renderRowCells(row as T)}
+                    {collapseTable && (
+                      <StyledTableCell key={`collapse-${(row as any).id}`}>
+                        <IconButton
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setOpenCollapse((prev: any[]) => ({
+                              ...prev,
+                              [(row as any).id]: !prev[(row as any).id],
+                            }));
+                          }}
+                        >
+                          <KeyboardArrowDown
+                            sx={{
+                              transform: isOpenCollapse[(row as any).id]
+                                ? 'rotate(180deg)'
+                                : 'rotate(0deg)',
+                              transitionDuration: '300ms',
+                              transitionTimingFunction: 'ease-in-out',
+                              transitionProperty: 'transform',
+                              pointerEvents: 'none',
+                            }}
+                          />
+                        </IconButton>
+                      </StyledTableCell>
+                    )}
+
+                    {rowCells(row as T)}
                   </TableRow>
-                  {renderCollapseRow && renderCollapseRow(row as T)}
+                  {collapseTable && (
+                    <TableRow>
+                      <StyledCollapseTableCell colSpan={9}>
+                        <Collapse in={isOpenCollapse[(row as any).id]} timeout="auto">
+                          {collapseTable(row as T)}
+                        </Collapse>
+                      </StyledCollapseTableCell>
+                    </TableRow>
+                  )}
                 </>
               );
             })}
